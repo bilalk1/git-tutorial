cf12f31cfdfeca870c79c19d3ea1c0415058d09d
bilal ahmad <bilaalahmaad1@gmail.com>
2022-07-14 11:25:07 +0500

    dsc

--- hello.js
+++ hello.js
@@ -1,2 +1,3 @@
 const introduction = "Hello My name is bilal!"
 const description = "Hello My name is bilal and I am a software consultant! I live in lahore."
+const description1 = "Hello My name is bilal and I am a software consultant! I live in lahore."
